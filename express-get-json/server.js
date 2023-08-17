b893c22853d08547643cd18dcff3d46e9c2b1b11
Edward Kim <ewakim90@gmail.com>
2023-08-17 06:45:59 +0000

    Draft

---
+++ express-get-json/server.js
@@ -0,0 +1,33 @@
+// import express from 'express'
+
+// const grades = {
+//   12: {
+//     id: 12,
+//     name: 'Tim Berners-Lee',
+//     course: 'HTML',
+//     score: 95,
+//   },
+//   47: {
+//     id: 47,
+//     name: 'Brendan Eich',
+//     course: 'JavaScript',
+//     score: 100,
+//   },
+//   273: {
+//     id: 273,
+//     name: 'Forbes Lindsay',
+//     course: 'JavaScript',
+//     score: 92,
+//   },
+// };
+
+// const app = express();
+
+// app.use((req, res) => {
+//   console.log(req.method);
+//   res.send('hello world');
+// });
+
+// app.listen(8080, () => {
+//   console.log('server is listening on port 8080');
+// });
