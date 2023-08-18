29e1f15f13ab873006b75ccfe0f77c2d2c703a0b
Edward Kim <ewakim90@gmail.com>
2023-08-18 03:31:19 +0000

    Completed express-get-json exercise.

--- express-get-json/express-get-json-quiz-notes.md
+++ express-get-json/express-get-json-quiz-notes.md
@@ -5,8 +5,9 @@
 After completing this exercise, you should be able to discuss or answer the following questions:
 
 - What is an API endpoint?
-
+  API endpoint is a specific location within an API that accepts requests and sends back responses.
 - What is the appropriate `Content-Type` header for HTTP messages that contain JSON in their bodies?
+  The correct content type for JSON is application/json, and the correct content type for JSONP (padded JSON) is application/javascript.
 
 ## Notes
 
--- express-get-json/server.js
+++ express-get-json/server.js
@@ -1,33 +1,36 @@
-// import express from 'express'
+import express from 'express';
 
-// const grades = {
-//   12: {
-//     id: 12,
-//     name: 'Tim Berners-Lee',
-//     course: 'HTML',
-//     score: 95,
-//   },
-//   47: {
-//     id: 47,
-//     name: 'Brendan Eich',
-//     course: 'JavaScript',
-//     score: 100,
-//   },
-//   273: {
-//     id: 273,
-//     name: 'Forbes Lindsay',
-//     course: 'JavaScript',
-//     score: 92,
-//   },
-// };
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92,
+  },
+};
 
-// const app = express();
+const app = express();
 
-// app.use((req, res) => {
-//   console.log(req.method);
-//   res.send('hello world');
-// });
+app.get('/api/grades', (req, res) => {
+  const Array = [];
+  for (const grade in grades) {
+    Array.push(grades[grade]);
+  }
+  res.json(Array);
+});
 
-// app.listen(8080, () => {
-//   console.log('server is listening on port 8080');
-// });
+app.listen(8080, () => {
+  console.log('server is listening on port 8080');
+});
